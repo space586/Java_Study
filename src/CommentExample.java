71db2657199d9ddde85435efd7a4ace99f519c2b
space586 <space586@hanmail.net>
2018-07-15 20:03:03 +0900

    Initial Rogan Java

---
+++ src/CommentExample.java
@@ -0,0 +1,35 @@
+
+/**
+ * API 생성용 주석입니다.
+ * 자신의 코드를 API 문서화시킬 때 사용합니다.
+ * @author hong
+ * @version 1.0
+ * @since 2018.07.11~
+ *
+ */
+
+public class CommentExample {
+	
+	//main이라고 쓰고 컨트롤+스페이스를 누르면 자동완성.
+	public static void main(String[] args) {
+		
+		//컨트롤 + F11을 누르면 컴파일 및 실행.
+		//println()은 출력한 뒤에 줄바꿈을 실행.
+		//System.out.println("hello~");
+		System.out.println("bye!!~");
+		//sysout + 컨트롤 + 스페이스: 출력문 자동완성.
+		System.out.println("hi~~!");
+		
+		
+		/*
+		안녕하세요. 수요일 저녁에 자바수업을
+		듣고 있습니다.
+		날씨가 매우 습합니다.
+		*/
+	}
+
+}
+
+
+
+
